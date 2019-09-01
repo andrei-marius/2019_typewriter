be1a8baf4e68d1487386dab54cef62be918a188c
Andrei Marius Hutupas Antoniu <andreimantoniu@yahoo.com>
2019-09-01 19:25:15 +0200

    working js added

--- typewriter.js
+++ typewriter.js
@@ -1,1 +1,35 @@
 "use strict";
+
+let aText = [];
+let htmlText = document.querySelector("#typewriter").textContent;
+aText.push(htmlText);
+let iSpeed = 200; 
+let iIndex = 0; 
+let iArrLength = aText[0].length; 
+let iScrollAt = 20; 
+let iTextPos = 0; 
+let sContents = ''; 
+let iRow; 
+ 
+function typewriter(){
+ sContents =  ' ';
+ iRow = Math.max(0, iIndex-iScrollAt);
+ let destination = document.getElementById("typewriter");
+ 
+ while ( iRow < iIndex ) {
+  sContents += aText[iRow++];
+ }
+ destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos);
+ if ( iTextPos++ == iArrLength ) {
+  iTextPos = 0;
+  iIndex++;
+  if ( iIndex != aText.length ) {
+   iArrLength = aText[iIndex].length;
+   setTimeout("typewriter()", 500);
+  }
+ } else {
+  setTimeout("typewriter()", iSpeed);
+ }
+}
+
+typewriter();
